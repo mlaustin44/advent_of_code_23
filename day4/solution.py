94248c36385048ebac0c8b47904cb7e61b428e28
Matthew Austin <matthew@aescape.co>
2023-12-05 22:43:36 -0800

    only took 3 minutes to run day 4 part 2...

--- day4/solution.py
+++ day4/solution.py
@@ -28,6 +28,30 @@ def solve_part1(cards):
     
     print(f"Part 1 solution: {total_pts}")
 
+def solve_part2(cards):
+    # make a card dictionary so we can look them up by number
+    card_dict = {}
+    for i, _ in enumerate(cards):
+        cards[i].append(i+1)
+        card_dict[i+1] = cards[i]
+    max_card = len(cards)
+    # run through the list, adding cards where needed
+    i = 0
+    while i < len(cards):
+        card = cards[i]
+        winners = 0
+        for n in card[1]:
+            if n in card[0]:
+                winners += 1
+        new_cards = list(range(card[2]+1, card[2]+winners+1))
+        for n in new_cards:
+            app_card = card_dict.get(n)
+            if app_card: cards.append(app_card)
+
+        i += 1
+
+    print(f"Part 2 solution is: {len(cards)}")
+
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         input_file = "input.test"
@@ -38,4 +62,5 @@ if __name__ == "__main__":
         input = f.readlines()
 
     cards = parse_input(input)
-    solve_part1(cards)
+    #solve_part1(cards)
+    solve_part2(cards)
